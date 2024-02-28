86e908ecd4249ed622c513fe3826aa01de2febac
Pedro Henri <99422315+Pedrenri@users.noreply.github.com>
2024-02-28 19:27:12 -0300

    feat: Commit inicial

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("05ad27c5-a63e-4e28-a28e-ad5a35e6441c");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produtos em Destaque" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
+export default HomePage;
